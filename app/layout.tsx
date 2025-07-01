18cef8f08ee6cb7085632262e2c9208a90768e15
Arish <Arish@2611>
2025-07-01 13:35:39 -0400

    Site title changed and logo added

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,9 +2,11 @@ import type { Metadata } from 'next'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'v0 App',
-  description: 'Created with v0',
-  generator: 'v0.dev',
+  title: "Arish Panjwani",
+  description: 'Created with <3',
+ icons: {
+    icon: "/logo.png"
+  }
 }
 
 export default function RootLayout({
